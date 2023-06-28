0ec8f3faab80472c737d313ef4de2085c3522287
thc <thc2cat@gmail.com>
2023-06-28 17:37:19 +0200

    v0.2 +split, +tests, +minlen + default bayesclass

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+// subayes : subject classification using bayesian filter
+// v0.1 : working draft.
+// v0.2 : minlen words, better split func, default class
+
 import (
 	"bufio"
 	"errors"
@@ -12,22 +16,27 @@ import (
 )
 
 var (
-	db, data            string
-	learnSpam, learnHam bool
-	Spam                bayesian.Class = "Spam"
-	Ham                 bayesian.Class = "Ham"
-	rxp                                = regexp.MustCompile(" |'|,|\t|\n")
+	db, data                     string
+	learnSpam, learnHam, verbose bool
+	Spam                         bayesian.Class = "Spam"
+	Ham                          bayesian.Class = "Ham"
+	words                                       = regexp.MustCompile("[\\p{L}]+")
+	minlength                                   = 4
 )
 
 func main() {
 
-	// db is path for storing data classes
+	// db is classes data store path
 	flag.StringVar(&db, "db", "db", " db path")
-	// data is the file to be read
+	// data is the file to be read when learning
 	flag.StringVar(&data, "d", "data", "data filename")
 	// choosing between learning Spam or Han (write db/classes files)
 	flag.BoolVar(&learnSpam, "learnSpam", false, "Learn Spam subjects")
 	flag.BoolVar(&learnHam, "learnHam", false, "Learn Ham subjects")
+	flag.IntVar(&minlength, "m", 4, "word min length")
+
+	flag.BoolVar(&verbose, "v", false, "verbose")
+
 	// Default is to read stdin line per line for classification
 	flag.Parse()
 
@@ -49,19 +58,19 @@ func main() {
 	case !learnHam && !learnSpam:
 		errcheck(K.ReadClassFromFile(Spam, db))
 		errcheck(K.ReadClassFromFile(Ham, db))
+		// Is it needed ? TF-IDF
+		K.ConvertTermsFreqToTfIdf()
+
 		showClassesCount(K)
 
 		scanner := bufio.NewScanner(os.Stdin)
 
 		for scanner.Scan() { // read line per line
 			text := scanner.Text()
-
 			if len(text) > 3 { // Minimum 'Re:'
-
-				spl := rxp.Split(text, -1)
-
+				spl := removeDuplicate(split(text), minlength)
 				fmt.Printf("%v: %s\n",
-					classify(K, spl),
+					classify(K, spl, Ham),
 					text)
 			}
 		}
@@ -76,40 +85,88 @@ func learn(c *bayesian.Classifier, xdb string, input string, class bayesian.Clas
 	err = c.ReadClassFromFile(class, xdb)
 	// if db/class don't exist, we will create it, so any err is acceptable
 	// errcheck(err)
+	// Better error handling should test error for acceptables ones
+	showClassesCount(c)
 
 	in, err := os.ReadFile(input) // in  type is []byte
 	errcheck(err)
 
 	ins := string(in) // ins type is string
 
-	indata := rxp.Split(ins, -1) // indata is []string
-	c.Learn(indata, class)
+	indata := split(ins) // indata is []string
+	indedup := removeDuplicate(indata, minlength)
+	c.Learn(indedup, class)
 
 	err = c.WriteClassToFile(class, xdb)
 	errcheck(err)
 
 	return nil
 }
-func classify(c *bayesian.Classifier, pattern []string) bayesian.Class {
+func classify(c *bayesian.Classifier, pattern []string, d bayesian.Class) bayesian.Class {
 	//  ProbScores return scores ([]float64), indexofclass, strict(?)
+	if len(pattern) == 0 { // return default class
+		return d
+	}
 	_, likelyb, _ := c.ProbScores(pattern)
 	return c.Classes[likelyb]
 }
 
 func showClassesCount(c *bayesian.Classifier) {
-	fmt.Printf("INFO classifier corpus : ")
-	for i := 0; i < len(c.Classes); i++ {
-		fmt.Printf(" [ %v -> %d items ]",
-			c.Classes[i],
-			c.WordCount()[i])
-
+	if !verbose {
+		return
 	}
-	fmt.Println()
+	fmt.Fprintf(os.Stderr, "INFO classifier corpus : ")
+	for i := 0; i < len(c.Classes); i++ {
+		if c.WordCount()[i] > 0 {
+			fmt.Fprintf(os.Stderr,
+				" [ %v -> %d items ]",
+				c.Classes[i],
+				c.WordCount()[i])
+		}
+	}
+	fmt.Fprintln(os.Stderr)
 }
 
 func errcheck(e error) {
 	if e != nil {
-		fmt.Println(e)
+		fmt.Fprintf(os.Stderr, "%v", e)
 		os.Exit(-1)
 	}
+}
+
+func split(s string) []string {
+	// 	return rxp.Split(s, -1)
+	// words := regexp.MustCompile("\\w+")
+	// words := regexp.MustCompile("\\P{M}+")
+	// words := regexp.MustCompile("[\\p{L}]+")
+	return words.FindAllString(s, -1)
+}
+
+// Generic version (string or int )
+// func removeDuplicate[T string | int](sliceList []T) []T {
+// 	allKeys := make(map[T]bool)
+// 	list := []T{}
+// 	for _, item := range sliceList {
+// 		if _, value := allKeys[item]; !value {
+// 			allKeys[item] = true
+// 			list = append(list, item)
+// 		}
+// 	}
+// 	return list
+// }
+
+// removeduplicate
+func removeDuplicate(sliceList []string, length int) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range sliceList {
+		if len(item) < length {
+			continue
+		}
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/navossoc/bayesian"
+)
+
+func Test_split(t *testing.T) {
+
+	tests := []struct {
+		name string
+		s    string
+		want []string
+	}{
+		// {"empty", "", []string{""}},
+		{"empty", "", nil},
+		{"A test !", "A test !", []string{"A", "test"}},
+		{"puctuation", "a lot of # numbers 1,2,5 ? ", []string{"a", "lot", "of", "numbers"}},
+		{"real1", "Re: [3616304] MODIFICATION // Fwd:  Senegal", []string{"Re", "MODIFICATION", "Fwd", "Senegal"}},
+		{"real2", "Chèque MAIF 1 000€", []string{"Chèque", "MAIF"}},
+		{"real3", "Re: Rattrapages (examens de seconde chance) 12-16 juin et 19-23  juin",
+			[]string{"Re", "Rattrapages", "examens", "de", "seconde", "chance", "juin", "et", "juin"}},
+		{"real4", "Vaše vlastné dievča lokálne", []string{"Vaše", "vlastné", "dievča", "lokálne"}},
+		{"real5", "Compañeras własne Zdobądź nära pobliżu",
+			[]string{"Compañeras", "własne", "Zdobądź", "nära", "pobliżu"}},
+	}
+	// TODO: Add test cases.
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := split(tt.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_most(t *testing.T) {
+
+	verbose = true
+	K := bayesian.NewClassifier(Ham, Spam)
+
+	errcheck(K.ReadClassFromFile(Spam, "db"))
+	errcheck(K.ReadClassFromFile(Ham, "db"))
+	showClassesCount(K)
+}
+
+func Test_removeDuplicate(t *testing.T) {
+	tests := []struct {
+		name   string
+		array  []string
+		want   []string
+		length int
+	}{
+		{"basic", []string{"AAAA", "AAAA", "BBB"}, []string{"AAAA", "BBB"}, 3},
+		{"length3", []string{"A", "BBB", "CCC", "BBB"}, []string{"BBB", "CCC"}, 3},
+		{"length2", []string{"AA", "BBB", "CCC", "BBB"}, []string{"AA", "BBB", "CCC"}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicate(tt.array, tt.length); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
